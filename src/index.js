e6d22cc9268373dccec3df15dcbac7e3834044a3
bradyzp <bradyzp@dynamicgravitysystems.com>
2022-07-09 10:58:57 -0600

    Fix async syntax

--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 
-try {
+async function main() {
+    try {
 
-    const githubToken = core.getInput('github_token')
-    const octokit = github.getOctokit(githubToken)
-    const { data: tags } = await octokit.rest.git.getRef({
-        ...github.context.repo,
-        ref: 'tags'
-    });
+        const githubToken = core.getInput('github_token')
+        const octokit = github.getOctokit(githubToken)
+        const { data: tags } = await octokit.rest.git.getRef({
+            ...github.context.repo,
+            ref: 'tags'
+        });
 
+        core.info(JSON.stringify(tags))
 
-    core.info(JSON.stringify(tags))
+        core.setOutput('version', '1.0.0')
 
-    core.setOutput('version', '1.0.0')
-
-} catch (error) {
-    core.setFailed(error.message)
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
+
+main();

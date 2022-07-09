b90fd57a51c6e805f3eb328945c21d7be90dda2a
bradyzp <bradyzp@dynamicgravitysystems.com>
2022-07-09 11:16:39 -0600

    WIP Iterating

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ const core = require('@actions/core')
 
 async function main() {
     try {
+        core.info('Attempting to resolve tags and version')
+
         const githubToken = core.getInput('github_token')
         const octokit = github.getOctokit(githubToken)
         const { data: tags } = await octokit.rest.git.getRef({
@@ -16,6 +18,7 @@ async function main() {
         core.setOutput('version', '1.0.0')
 
     } catch (error) {
+        core.error(JSON.stringify(error))
         core.setFailed(error.message)
     }
 }

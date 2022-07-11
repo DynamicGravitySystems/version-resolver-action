1f62c1dbfcc29b6ebf9e5c914d5f956139c86bf1
bradyzp <bradyzp@dynamicgravitysystems.com>
2022-07-10 19:58:02 -0600

    WIP Iterating

--- src/main.js
+++ src/main.js
@@ -2,63 +2,73 @@ const github = require('@actions/github')
 const core = require('@actions/core')
 
 
+async function getTags(page = 1) {
+    const githubToken = core.getInput('github_token')
+    const octokit = github.getOctokit(githubToken)
+
+    const {data: tags} = await octokit.request('GET /repos/{owner}/{repo}/tags', {
+        ...github.context.repo,
+        accept: 'application/vnd.github+json',
+        per_page: 100,
+        page: page
+    })
+
+    if (tags.size >= 100) {
+        return tags.concat(await getTags(page + 1))
+    } else {
+        return tags
+    }
+}
+
 async function run() {
     try {
         core.info('Attempting to resolve tags and version')
 
-        const githubToken = core.getInput('github_token')
-        const octokit = github.getOctokit(githubToken)
-
-        const {data: tags} = await octokit.request('GET /repos/{owner}/{repo}/tags', {
-            ...github.context.repo,
-            per_page: 100
-        })
+        const tags = getTags()
         const releaseType = core.getInput('semantic_release')
 
-
-        console.log('Context:')
-        console.log(JSON.stringify(github.context))
-
         if (tags.size === 0) {
-            core.info("No tags found for repo, returning default version 1.0.0")
+            const initVersion = core.getInput('init_version')
+            let ver = initVersion === '' ? '1.0.0' : initVersion
+            core.debug(`No tags found for repo, returning default version ${ver}`)
 
-            core.setOutput('version', '1.0.0')
+            core.setOutput('version', ver)
         } else {
             let maj = 0
             let min = 0
             let patch = 0
 
-            tags.map(t => {
-                return t.name.replace("v", "")
-            }).forEach(t => {
-                let parts = t.split('\.').map(it => {
-                    return parseInt(String(it));
+            tags.filter(t => t.name.startsWith("v"))
+                .map(t => t.name.replace("v", ""))
+                .forEach(t => {
+                    let parts = t.split('\.').map(it => {
+                        return parseInt(String(it));
+                    })
+
+                    let a = parts.shift()
+                    let b = parts.shift()
+                    let c = parts.shift()
+
+                    if (a !== undefined && a > maj) {
+                        maj = a
+                        min = b !== undefined ? b : 0
+                        patch = c !== undefined ? c : 0
+                        return
+                    }
+
+                    if (b !== undefined && b > min) {
+                        min = b
+                        patch = c !== undefined ? c : 0
+                        return
+                    }
+
+                    if (c !== undefined && c > patch) {
+                        patch = c
+                    }
                 })
 
-                let a = parts.shift()
-                let b = parts.shift()
-                let c = parts.shift()
-
-                if (a !== undefined && a > maj) {
-                    maj = a
-                    min = b !== undefined ? b : 0
-                    patch = c !== undefined ? c : 0
-                    return
-                }
-
-                if (b !== undefined && b > min) {
-                    min = b
-                    patch = c !== undefined ? c : 0
-                    return
-                }
-
-                if (c !== undefined && c > patch) {
-                    patch = c
-                }
-            })
-
-            console.log(`Resolved highest version ${maj}.${min}.${patch}`)
-            if(releaseType === 'major') {
+            core.info(`Resolved highest version ${maj}.${min}.${patch}`)
+            if (releaseType === 'major') {
                 maj++
             } else if (releaseType === 'patch') {
                 patch++
